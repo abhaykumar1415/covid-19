b7d601018f51425229ff383f564bc0984fe7a96b
Abhay Kumar <abhaykumar@Abhays-MacBook-Pro.local>
2020-04-01 16:19:16 +0530

    Added db name in config

--- src/env/development.ts
+++ src/env/development.ts
@@ -3,6 +3,6 @@
 export const envConfig: any = {
     database: {
         MONGODB_URI: 'mongodb://localhost:27017/',
-        MONGODB_DB_MAIN: 'dev_DB'
+        MONGODB_DB_MAIN: 'covid_DB'
     }
 };
